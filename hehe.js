e82a2f38b6e297e0db0cde851a639845345eedec
coconut-doggy <154691143+coconut-doggy@users.noreply.github.com>
2025-06-24 11:49:22 -0400

    Create hehe.js

---
+++ hehe.js
@@ -0,0 +1,10 @@
+ function getRandomInt(max) {
+            return math.floor(math.random() * max);
+      }
+      function lol(){
+        i=getRandomInt(4);
+        songs=["AntonioVivaldi_Spring.mp3", "05 Edvard Grieg - Peer Gynt, Morning Mood, 1875.mp3", "Classic Easter- Axs Music (2).mp3", "05. Franz Joseph Haydn - Trumpet Concerto.mp3"];
+        songP = songs[i];
+
+        return(songP);
+       }

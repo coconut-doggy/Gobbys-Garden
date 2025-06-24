3dc1970e77e330bec4e37b495ef433476e854a18
Logan Silva <logan.edward.silva@gmail.com>
2025-06-24 15:26:41 -0400

    Bugfixes?

--- hehe.js
+++ hehe.js
@@ -1,10 +1,8 @@
- function getRandomInt(max) {
-            return math.floor(math.random() * max);
-      }
-      function lol(){
-        i=getRandomInt(4);
-        songs=["https://github.com/coconut-doggy/Gobbys-Garden/blob/5705a65ec00321b0691bca8c116bafbbc31fc7bc/AntonioVivaldi_Spring.mp3", "https://github.com/coconut-doggy/Gobbys-Garden/blob/5705a65ec00321b0691bca8c116bafbbc31fc7bc/05%20Edvard%20Grieg%20-%20Peer%20Gynt%2C%20Morning%20Mood%2C%201875.mp3", "https://github.com/coconut-doggy/Gobbys-Garden/blob/5705a65ec00321b0691bca8c116bafbbc31fc7bc/Classic%20Easter-%20Axs%20Music%20(2).mp3", "https://github.com/coconut-doggy/Gobbys-Garden/blob/5705a65ec00321b0691bca8c116bafbbc31fc7bc/05.%20Franz%20Joseph%20Haydn%20-%20Trumpet%20Concerto.mp3"];
-        songP = songs[i];
-
-        return(songP);
-       }
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+function lol() {
+  //songs = ["https://github.com/coconut-doggy/Gobbys-Garden/blob/5705a65ec00321b0691bca8c116bafbbc31fc7bc/AntonioVivaldi_Spring.mp3", "https://github.com/coconut-doggy/Gobbys-Garden/blob/5705a65ec00321b0691bca8c116bafbbc31fc7bc/05%20Edvard%20Grieg%20-%20Peer%20Gynt%2C%20Morning%20Mood%2C%201875.mp3", "https://github.com/coconut-doggy/Gobbys-Garden/blob/5705a65ec00321b0691bca8c116bafbbc31fc7bc/Classic%20Easter-%20Axs%20Music%20(2).mp3", "https://github.com/coconut-doggy/Gobbys-Garden/blob/5705a65ec00321b0691bca8c116bafbbc31fc7bc/05.%20Franz%20Joseph%20Haydn%20-%20Trumpet%20Concerto.mp3"];
+  songs = ["AntonioVivaldi_Spring.mp3", "05%20Edvard%20Grieg%20-%20Peer%20Gynt%2C%20Morning%20Mood%2C%201875.mp3", "Classic%20Easter-%20Axs%20Music%20(2).mp3", "05.%20Franz%20Joseph%20Haydn%20-%20Trumpet%20Concerto.mp3"];
+  return (songs[getRandomInt(4)]);
+}

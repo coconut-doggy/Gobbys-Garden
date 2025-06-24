2565dceb2bd24cabb2b7eb5cddd2f42d06cde1aa
Logan Silva <logan.edward.silva@gmail.com>
2025-06-24 15:37:44 -0400

    Song working?

--- hehe.js
+++ hehe.js
@@ -6,3 +6,16 @@ function lol() {
   songs = ["AntonioVivaldi_Spring.mp3", "05%20Edvard%20Grieg%20-%20Peer%20Gynt%2C%20Morning%20Mood%2C%201875.mp3", "Classic%20Easter-%20Axs%20Music%20(2).mp3", "05.%20Franz%20Joseph%20Haydn%20-%20Trumpet%20Concerto.mp3"];
   return (songs[getRandomInt(4)]);
 }
+
+function play_song() {
+  songs = ["https://github.com/coconut-doggy/Gobbys-Garden/blob/5705a65ec00321b0691bca8c116bafbbc31fc7bc/AntonioVivaldi_Spring.mp3", "https://github.com/coconut-doggy/Gobbys-Garden/blob/5705a65ec00321b0691bca8c116bafbbc31fc7bc/05%20Edvard%20Grieg%20-%20Peer%20Gynt%2C%20Morning%20Mood%2C%201875.mp3", "https://github.com/coconut-doggy/Gobbys-Garden/blob/5705a65ec00321b0691bca8c116bafbbc31fc7bc/Classic%20Easter-%20Axs%20Music%20(2).mp3", "https://github.com/coconut-doggy/Gobbys-Garden/blob/5705a65ec00321b0691bca8c116bafbbc31fc7bc/05.%20Franz%20Joseph%20Haydn%20-%20Trumpet%20Concerto.mp3"];
+  i = Math.floor(Math.random() * songs.length);
+  song_url = songs[i];
+  console.log("Playing song: " + song_url);
+  var audio = new Audio(song_url);
+  //var audio = new Audio("https://raw.githubusercontent.com/coconut-doggy/Gobbys-Garden/5705a65ec00321b0691bca8c116bafbbc31fc7bc/AntonioVivaldi_Spring.mp3");
+  audio.play();
+  audio.loop = true;
+  audio.volume = 0.2;
+  return audio;
+}
